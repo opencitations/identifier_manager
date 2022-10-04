552e0ede15265a93ad765009830e87a79d8f4e06
ariannamorettj <arianna.moretti4@unibo.it>
2022-10-05 00:05:22 +0200

    exists update in doi Manager
    Default positive behavior in case API access is not enabled

--- oc_idmanager/doi.py
+++ oc_idmanager/doi.py
@@ -99,8 +99,8 @@ class DOIManager(IdentifierManager):
                 return False
 
         if get_extra_info:
-            return False, {"valid": False}
-        return False
+            return True, {"valid": True}
+        return True
 
     def extra_info(self, api_response):
         result = {}
